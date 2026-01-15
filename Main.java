2fd831e79ba0c7b6c753e5d48945913d94002136
Senuthi9 <sskottegoda@students.nsbm.ac.lk>
2026-01-15 21:40:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.mycompany.sis;
+
+import com.mycompany.sis.auth.LoginFrame; //  FIXED
+
+public class Main {
+    public static void main(String[] args) {
+        new LoginFrame().setVisible(true);
+    }
+}
